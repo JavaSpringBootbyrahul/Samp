3de9dfafb534a3c678abe55cc12bf54bc1ab88d9
RAHUL-PC\Rzope <rzope5923@gmail.com>
2022-12-15 12:14:01 +0530

    commit:IDS233-print() method commit

--- MyFile.java
+++ MyFile.java
@@ -1,10 +1,15 @@
 public class MyFile {
 	public static void main(String args[]) {
 		System.out.println("MyFile");
-		display();
+		// display();
 	}
-	static void display()
-	{	
-		System.out.println("Hello");
+	// static void display()
+	// {
+	// System.out.println("Hello");
+	// }
+
+	static void print() {
+		// First task
+		System.out.println("Print method compeleted");
 	}
 }
