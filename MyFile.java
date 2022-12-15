936b84062012fd2b9eec5727c574b8118ecf0287
RAHUL-PC\Rzope <rzope5923@gmail.com>
2022-12-15 11:30:15 +0530

    new method()code commit

--- MyFile.java
+++ MyFile.java
@@ -1,7 +1,10 @@
-public class MyFile
-{
-	public static void main(String args[])
-	{
+public class MyFile {
+	public static void main(String args[]) {
 		System.out.println("MyFile");
+		// System.out.println("Hellowoeld");
+	}
+
+	public void main() {
+		System.out.println("hello");
 	}
 }
