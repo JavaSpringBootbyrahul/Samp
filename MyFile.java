71037ed0d2f5883e1a7421678367d9f7a579c862
RAHUL-PC\Rzope <rzope5923@gmail.com>
2022-12-15 13:27:28 +0530

    commit MyFile.java stashed changes

--- MyFile.java
+++ MyFile.java
@@ -1,15 +1,7 @@
 public class MyFile {
 	public static void main(String args[]) {
 		System.out.println("MyFile");
-		display();
+
 	}
 
-	static void display() {
-		System.out.println("Hello");
-	}
-
-	static void print() {
-
-		System.out.println("Print method compeleted");
-	}
 }
