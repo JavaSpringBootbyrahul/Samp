bcd7057a109e71d5968d6ca7e10859644dee98b1
Rahul Zope <99934293+JavaSpringBootbyrahul@users.noreply.github.com>
2022-12-15 11:56:23 +0530

    update:ID222-display() method commit

--- MyFile.java
+++ MyFile.java
@@ -1,6 +1,10 @@
 public class MyFile {
 	public static void main(String args[]) {
 		System.out.println("MyFile");
-		
+		display();
+	}
+	static void display()
+	{	
+		System.out.println("Hello");
 	}
 }
